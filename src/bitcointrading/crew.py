dd414b0a5f0948fc5b39654cbb8b5fc100796bbc
emirardagn <arda.gun.24752@ozu.edu.tr>
2025-04-15 14:25:45 +0300

    deleted:    src/bitcointrading/__init__.py 	modified:   src/bitcointrading/config/agents.yaml 	modified:   src/bitcointrading/config/tasks.yaml 	modified:   src/bitcointrading/crew.py 	modified:   src/bitcointrading/main.py

--- src/bitcointrading/crew.py
+++ src/bitcointrading/crew.py
@@ -2,57 +2,57 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from src.bitcointrading.tools.technical_tools.indicators import CALCULATE_INDICATORS
 from src.bitcointrading.tools.research_tools.scraping import GET_NEWS
+import yaml
+
 @CrewBase
-class Bitcointrading():
+class BitcoinTrading():
     """Bitcoin Trading Analysis Crew"""
 
-    agents_config = 'src/bitcointrading/config/agents.yaml'
-    tasks_config = 'src/bitcointrading/config/tasks.yaml'
-
-    get_news = GET_NEWS()
-    calculate_indicators = CALCULATE_INDICATORS()
-    
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+ 
     @agent
-    def researcher(self) -> Agent:
+    def research_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'],
+            config=self.agents_config["research_agent"],
             verbose=True,
-            tools=[self.get_news]
         )
 
     @agent
-    def financial_analyst(self) -> Agent:
+    def technical_analyst(self) -> Agent:
         return Agent(
-            config=self.agents_config['financial_analyst'],
+            config=self.agents_config["technical_analyst_agent"],
             verbose=True,
-            tools=[self.calculate_indicators]
         )
 
     @agent
     def decider(self) -> Agent:
         return Agent(
-            config=self.agents_config['decider'],
+            config=self.agents_config["decider_agent"],
             verbose=True
         )
 
     @task
     def research_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'],
+            config=self.tasks_config["researcher_task"],
+            agent=self.research_agent(),
             output_file='market_research.md',
         )
 
     @task
     def technical_analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config['technical_analysis_task'],
+            config=self.tasks_config["technical_analysis_task"],
+            agent=self.technical_analyst(),
             output_file='technical_analysis.md',
         )
 
     @task
     def decision_task(self) -> Task:
         return Task(
-            config=self.tasks_config['decision_task'],
+            config=self.tasks_config["decision_task"],
+            agent=self.decider(),
             output_file='trading_decision.md'
         )
 
--- src/bitcointrading/main.py
+++ src/bitcointrading/main.py
@@ -1,24 +1,10 @@
-#!/usr/bin/env python
-import sys
-import warnings
+from bitcointrading.crew import BitcoinTrading
 
-from datetime import datetime
-
-from bitcointrading.crew import Bitcointrading
-
-warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
 def run():
-    """
-    Run the crew.
-    """
-    inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
-    }
     
     try:
-        Bitcointrading().crew().kickoff(inputs=inputs)
+        BitcoinTrading().crew().kickoff()
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
